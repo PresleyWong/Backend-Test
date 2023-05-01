a1bcde33c31484ab258e04bab7436268c897fb2f
PresleyWong <presley_wg@yahoo.com>
2023-05-01 15:26:33 +0800

    added improved RHM

--- Q2/LoadTest.py
+++ Q2/LoadTest.py
@@ -5,6 +5,7 @@ import time
 def load_test(n=20):
     for _ in range(n):
         response = requests.get('http://127.0.0.1:8000/get-hash-odd-number')
+        # response = requests.get('http://127.0.0.1:8000/get-hash-odd-number-v2')
         print(response.json())
         time.sleep(1)
 
--- Q2/main.py
+++ Q2/main.py
@@ -5,6 +5,7 @@ import random
 import time
 from fastapi import FastAPI
 from threading import Thread
+import multiprocessing
 
 app = FastAPI()
 
@@ -49,6 +50,52 @@ async def get_hash_odd_number():
     end_time = time.time()
     return {"hash": response["hash"], "time_elapsed": end_time - start_time}
 
+
+## Improved Random Hash Machine ###
+def random_hash_generator(procnum, hash_dict):
+    text = random_string_generator()
+    hash = hashlib.sha256(text.encode('UTF-8')).hexdigest()
+    hash_dict[procnum] = hash
+
+
+@app.get("/get-random-hash-v2")
+async def get_sha256_hash_v2():
+    start_time = time.time()
+    manager = multiprocessing.Manager()
+    hash_dict = manager.dict()
+    jobs = []
+
+    for i in range(multiprocessing.cpu_count()):
+        p = Thread(target=random_hash_generator, args=(i, hash_dict))
+        jobs.append(p)
+        p.start()
+
+    for proc in jobs:
+        proc.join()
+
+    time.sleep(1)
+    end_time = time.time()
+
+    return {"hash_list": hash_dict.values(), "time_elapsed": end_time - start_time}
+
+
+@app.get("/get-hash-odd-number-v2")
+async def get_hash_odd_number_v2():
+    start_time = time.time()
+    found_flag = False
+    while not found_flag:
+        response = await get_sha256_hash_v2()
+
+        for hash in response["hash_list"]:
+            last_character = hash[-1]
+
+            if last_character.isdigit() and int(last_character) % 2 != 0:
+                found_flag = True
+                break
+
+    time.sleep(1)
+    end_time = time.time()
+    return {"hash": hash, "time_elapsed": end_time - start_time}
 
 
 if __name__ == "__main__":
